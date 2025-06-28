fa01f4b774518ea519511b6087b36cabedf83b53
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-06-28 13:01:36 +0000

    Header Updated;

--- header.php
+++ header.php
@@ -20,14 +20,8 @@
         require_once 'navi.php'; // Include your configuration file if needed
         ?>
         <!-- Header-->
-        <header class="py-5">
-            <div class="container px-lg-5">
-                <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
-                    <div class="m-4 m-lg-5">
-                        <h1 class="display-5 fw-bold">A warm welcome!</h1>
-                        <p class="fs-4">Bootstrap utility classes are used to create this jumbotron since the old component has been removed from the framework. Why create custom CSS when you can use utilities?</p>
-                        <a class="btn btn-primary btn-lg" href="#!">Call to action</a>
-                    </div>
-                </div>
-            </div>
+         <header class="header-text header-background py-5 p-4 mb-5 p-lg-5 bg-light rounded-3 text-center">
+            <h1 class="display-5 fw-bold">welcome!</h1>
+            <p class="fs-4">Solutions HUB</p>
+            <a class="btn btn-primary btn-lg" href="contact.php">Contact</a>
         </header>
