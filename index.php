1fb2aa4fd04ab6a7db012b7918a0cc2dd0e8881d
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-06-28 12:02:50 +0000

    Main Navigations URLs updated;

--- index.php
+++ index.php
@@ -14,20 +14,11 @@
         <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body>
-        <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container px-lg-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php
+        // This is a simple PHP script to demonstrate the use of PHP in a web page.
+        // You can add your PHP code here to handle form submissions, database connections, etc.
+        require_once 'navi.php'; // Include your configuration file if needed
+        ?>
         <!-- Header-->
         <header class="py-5">
             <div class="container px-lg-5">
